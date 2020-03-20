4db53a89b425db2b9a257fa582bf0ba9aa93adf2
ljun1118 <ejun1118@gmail.com>
2020-03-20 16:34:50 +0900

    코드업 1002~1010

---
+++ src/CodeUp/codeUp1010.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class codeUp1010 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        System.out.println(n);
+    }
+}
