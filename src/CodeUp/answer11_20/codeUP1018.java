baecd0b3bcb220cf1d45913260a7cc57878179b0
ljun1118 <ejun1118@gmail.com>
2020-03-23 11:39:38 +0900

    Add: 1017,1018

---
+++ src/CodeUp/answer11_20/codeUP1018.java
@@ -0,0 +1,10 @@
+package CodeUp.answer11_20;
+
+import java.util.Scanner;
+
+public class codeUP1018 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextLine());
+    }
+}
---
+++ src/CodeUp/answer11_20/codeUp1017.java
@@ -0,0 +1,11 @@
+package CodeUp.answer11_20;
+
+import java.util.Scanner;
+
+public class codeUp1017 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a + " " + a + " " + a);
+    }
+}
