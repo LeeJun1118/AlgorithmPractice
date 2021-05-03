8c6a68b64b5a9471cba3824531437fbe535e66a5
ljun1118 <ejun1118@gmail.com>
2021-05-03 11:22:44 +0900

    ADD : 2588

---
+++ src/BeakJun/basic/b_2588.java
@@ -0,0 +1,16 @@
+package BeakJun.basic;
+
+import java.util.Scanner;
+
+public class b_2588 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A*(B%10));
+        System.out.println(A*(B/10%10));
+        System.out.println(A*(B/100%10));
+        System.out.println(A*B);
+    }
+}
