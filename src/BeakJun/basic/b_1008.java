de902d90dd4240d8c72f9896bb39e52f07d42a71
ljun1118 <ejun1118@gmail.com>
2021-05-03 10:36:04 +0900

    ADD : 1008

---
+++ src/BeakJun/basic/b_1008.java
@@ -0,0 +1,12 @@
+package BeakJun.basic;
+
+import java.util.Scanner;
+
+public class b_1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+        System.out.print(A/B);
+    }
+}
