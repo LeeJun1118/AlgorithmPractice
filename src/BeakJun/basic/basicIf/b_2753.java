1696b9389962fd79aee9c5df84c1bb2361d3e661
ljun1118 <ejun1118@gmail.com>
2021-05-04 11:12:46 +0900

    ADD : 2753 - leap year

---
+++ src/BeakJun/basic/basicIf/b_2753.java
@@ -0,0 +1,16 @@
+package BeakJun.basic.basicIf;
+
+import java.util.Scanner;
+
+public class b_2753 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        if (year%4==0&&year%100!=0)
+            System.out.println("1");
+        else if (year%400==0)
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
