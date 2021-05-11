1492dbe7964c26518725680f13721e9fdcf69565
ljun1118 <ejun1118@gmail.com>
2021-05-11 10:15:51 +0900

    ADD : 10952 - while loop

---
+++ src/BeakJun/basic/basicFor/b_10952.java
@@ -0,0 +1,18 @@
+package BeakJun.basic.basicFor;
+
+
+import java.util.Scanner;
+
+public class b_10952 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            if(A == 0 && B == 0)
+                break;
+            System.out.println(A+B);
+        }
+    }
+}
