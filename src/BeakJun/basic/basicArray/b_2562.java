ace5f672d4b4e80ec4340fad47f0e19ca2baf39c
ljun1118 <ejun1118@gmail.com>
2021-05-17 11:10:36 +0900

    ADD : 2562

---
+++ src/BeakJun/basic/basicArray/b_2562.java
@@ -0,0 +1,26 @@
+package BeakJun.basic.basicArray;
+
+import java.util.Scanner;
+
+public class b_2562 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] N = new int[9];
+
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < N.length; i++) {
+            N[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < N.length; i++) {
+            if (max < N[i]){
+                max = N[i];
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index + 1);
+    }
+}
