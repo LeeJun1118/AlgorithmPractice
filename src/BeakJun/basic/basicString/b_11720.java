316d48216734359a6d9f6c854db08963f93569f5
ljun1118 <ejun1118@gmail.com>
2021-05-24 10:54:27 +0900

    ADD : 11720 - Complete : Using Scanner

--- src/BeakJun/basic/basicString/b_11720.java
+++ src/BeakJun/basic/basicString/b_11720.java
@@ -1,31 +1,20 @@
 package BeakJun.basic.basicString;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class b_11720 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.nextLine();
+
+        String num = scanner.nextLine();
+        String[] sp = num.split("");
 
         int sum = 0;
-
         for (int i = 0; i < N; i++) {
-            char c = (char) br.read();
-            int j = c;
-            sum = sum + c;
+            sum += Integer.parseInt(sp[i]);
         }
-
-
-        /*for (int i = 0; i < s.split("").length; i++) {
-            String[] c = s.split("");
-            sum = sum + Integer.parseInt(c[i]);
-        }
-*/
-        br.close();
         System.out.println(sum);
     }
 }
