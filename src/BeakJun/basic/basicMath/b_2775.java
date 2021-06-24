dcb0d10f344fc6c0c7bcedec2bb30b7c28dacc60
ijun <ljun1118@ks.ac.kr>
2021-06-24 15:59:20 +0900

    Modify : 2775 - Basic Math : 부녀회장이 될테야 - 완전히 맞는 코드로 고쳤음

--- src/BeakJun/basic/basicMath/b_2775.java
+++ src/BeakJun/basic/basicMath/b_2775.java
@@ -1,7 +1,6 @@
 package BeakJun.basic.basicMath;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class b_2775 {
     public static void main(String[] args) throws IOException {
@@ -9,23 +8,22 @@ public class b_2775 {
         int T = Integer.parseInt(br.readLine());
         int[][] apt = new int[15][15];
 
-        //1행 : 1 2 3 4 5 ~
+        //0층 : 1 2 3 4 5호 ~
+        //0호 : 1
         for (int j = 0; j < 15; j++) {
-            apt[0][j] = j;
-            apt[j][1] = 1;
+            apt[j][0] = 1; //j층 0호
+            apt[0][j] = j + 1; //0층 j호
         }
 
         for (int j = 1; j < 15; j++) {
-            for (int l = 2; l < 15; l++) {
+            for (int l = 1; l < 15; l++) {
                 apt[j][l] = apt[j][l - 1] + apt[j - 1][l];
             }
         }
         for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-            System.out.println(apt[k][n]);
+            System.out.println(apt[k][n-1]);
         }
-        System.out.println(Arrays.deepToString(apt));
     }
-
 }
