615790896d03ffd70eee6bf49f1275221c705791
LeeJun <ejun1118@gmail.com>
2021-07-02 17:38:03 +0900

    ADD : 1101 - 하는 중

---
+++ src/BeakJun/basic/basicMath/b_1011.java
@@ -0,0 +1,22 @@
+package BeakJun.basic.basicMath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b_1011 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+
+        }
+
+
+
+    }
+}
