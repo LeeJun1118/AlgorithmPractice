1ae259720a9064b765acba7d9b3a33f2d1d558a6
LeeJun <ejun1118@gmail.com>
2021-07-07 20:00:17 +0900

    진행상태 업데이트

--- src/BeakJun/basic/basicMath2/b_1978.java
+++ src/BeakJun/basic/basicMath2/b_1978.java
@@ -3,21 +3,34 @@ package BeakJun.basic.basicMath2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class b_1978 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int count = 0;
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            System.out.println("Num : " + num);
-            if (num / 2 != 0 || num / 3 != 0)
+        String st = br.readLine();
+        String[] str = st.split(" ");
+
+        for (String s : str) {
+            int num = Integer.parseInt(s);
+            boolean isPrime = true;
+            if (num == 1) {
+                continue;
+            } else if (num == 2) {
+                count++;
+                continue;
+            } else {
+                for (int j = 2; j < num; j++) {
+                    if (num % j == 0) {
+                        isPrime = false;
+                    }
+                }
+            }
+            if (isPrime)
                 count++;
         }
-
         System.out.println(count);
     }
 }
