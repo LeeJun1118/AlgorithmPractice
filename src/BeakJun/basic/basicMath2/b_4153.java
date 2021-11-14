4685db4b4e1a2931c5246c88d84e2e87e1fc0dae
LeeJun <ejun1118@gmail.com>
2021-11-14 17:23:45 +0900

    Complete : 4153

--- src/BeakJun/basic/basicMath2/b_4153.java
+++ src/BeakJun/basic/basicMath2/b_4153.java
@@ -1,7 +1,28 @@
 package BeakJun.basic.basicMath2;
 
-public class b_4153 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class b_4153 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str;
+        int a,b,c;
+        while (true){
+            str = br.readLine().split(" ");
+            if (str[0].equals("0") && str[1].equals("0") && str[2].equals("0"))
+                break;
+            else {
+                a = Integer.parseInt(str[0]) * Integer.parseInt(str[0]);
+                b = Integer.parseInt(str[1]) * Integer.parseInt(str[1]);
+                c = Integer.parseInt(str[2]) * Integer.parseInt(str[2]);
+
+                if (a + b == c || a + c == b || b + c == a)
+                    System.out.println("right");
+                else
+                    System.out.println("wrong");
+            }
+        }
     }
 }
