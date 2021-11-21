b40d4ad0b3365852c9e32668d41834bdda568e94
LeeJun <ejun1118@gmail.com>
2021-11-21 22:54:59 +0900

    Compelete

--- src/BeakJun/basic/basicMath2/b_1002.java
+++ src/BeakJun/basic/basicMath2/b_1002.java
@@ -22,48 +22,31 @@ public class b_1002 {
         for (int i = 0; i < loop; i++) {
             arr = bufferedReader.readLine().split(" ");
 
-            // 각각의 좌표
-            double jxAbs = Math.abs(Double.parseDouble(arr[0]));
-            double jyAbs = Math.abs(Double.parseDouble(arr[1]));
-            double bxAbs = Math.abs(Double.parseDouble(arr[3]));
-            double byAbs = Math.abs(Double.parseDouble(arr[4]));
+            int jx = Integer.parseInt(arr[0]);
+            int jy = Integer.parseInt(arr[1]);
+            int jr = Integer.parseInt(arr[2]);
 
-            double jx = Double.parseDouble(arr[0]);
-            double jy = Double.parseDouble(arr[1]);
-            double jr = Double.parseDouble(arr[2]);
-            double bx = Double.parseDouble(arr[3]);
-            double by = Double.parseDouble(arr[4]);
-            double br = Double.parseDouble(arr[5]);
-
-            // J,B 사이의 거리 구하기
-            double jb_x = Math.abs(jxAbs - bxAbs);
-            double jb_y = Math.abs(jyAbs - byAbs);
+            int bx = Integer.parseInt(arr[3]);
+            int by = Integer.parseInt(arr[4]);
+            int br = Integer.parseInt(arr[5]);
 
             // r 과의 거리 합
-            double r_sum = Integer.parseInt(arr[2]) + Integer.parseInt(arr[5]);
+            int dis = (int)(Math.pow(jx - bx,2) + Math.pow(jy - by,2));
 
             // 둘의 좌표가 같을 때
-            if (jx == bx && jy == by) {
-                if (jr == br)
-                    System.out.println(-1);
-                else
-                    System.out.println(0);
-            }
-            //좌표가 다를 때
-            else {
-                // 한 원이 다른 원 안에 있는데 R 이 서로 다를 때
-                if (jxAbs + jr > bxAbs + br && jyAbs + jr > byAbs + br)
-                    System.out.println(0);
-                // 내접할 때
-                else if ((jxAbs + jr == bxAbs + br || jyAbs + jr > byAbs + br) || (jxAbs + jr > bxAbs + br || jyAbs + jr == byAbs + br))
-                    System.out.println(1);
-                // 겹치는 곳이 두 곳 생길 때
-                else if ((jx + jr > bx - br || jy + jr > by - br) || (bx + br > jx - jr || by + br > jy - jr))
-                    System.out.println(2);
-                // 외접할때 코드 작성 필요함
-                else
-                    System.out.println(0);
-            }
+            if (jx == bx && jy == by && jr == br)
+                System.out.println(-1);
+            else if (dis > Math.pow(jr + br,2))
+                System.out.println(0);
+            else if (dis < Math.pow(jr - br,2))
+                System.out.println(0);
+            else if (dis == Math.pow(jr - br,2))
+                System.out.println(1);
+            else if (dis == Math.pow(jr + br,2))
+                System.out.println(1);
+            else
+                System.out.println(2);
+
         }
     }
 }
