0d772ee5c321aad2c31725b780f4b4bb7e7519d5
LeeJun <ejun1118@gmail.com>
2022-01-07 22:16:20 +0900

    Complete : Fly me to the Alpha Centauri

--- src/BeakJun/basic/basicMath/b_1011_flyMeToTheAlphaCentauri.java
+++ src/BeakJun/basic/basicMath/b_1011_flyMeToTheAlphaCentauri.java
@@ -3,26 +3,33 @@ package BeakJun.basic.basicMath;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class b_1011_flyMeToTheAlphaCentauri {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader location;
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-            int result = 0;
 
-            location = new BufferedReader(new InputStreamReader(System.in));
-            String[] s = location.readLine().split(" ");
-            int x = Integer.parseInt(s[0]);
-            int y = Integer.parseInt(s[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             int distance = y - x;
 
+            int max = (int) (Math.sqrt(distance));
 
+            if (max == Math.sqrt(distance)){
+                System.out.println(2 * max -1);
+            }
+            else if (distance <= max * (max + 1)) {
+                System.out.println(2 * max);
+            }
+            else  {
+                System.out.println(2 * max + 1);
+            }
 
-
-            System.out.println(result);
         }
     }
 }
