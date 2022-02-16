7f194ec209d8ab67be37f0137cdcfc21cec79a55
ijun <ljun1118@ks.ac.kr>
2022-02-16 21:37:45 +0900

    프로그래머스 문제 추가 complete : 로또

---
+++ src/Programmers/level_1/lotto.java
@@ -0,0 +1,40 @@
+package Programmers.level_1;
+
+public class lotto {
+    public static void main(String[] args) {
+        int[] lottos = {1, 2, 3, 4, 5, 6};
+        int[] win_nums = {5, 3, 7, 8, 9, 3};
+
+        solution(lottos, win_nums);
+
+    }
+
+    private static int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        int zero = 0;
+        int count = 0;
+        for (int num : lottos) {
+            if (num == 0)
+                zero++;
+            for (int win_num : win_nums) {
+                if (num == win_num) {
+                    count++;
+                }
+            }
+        }
+        if (zero == 6)
+        {
+            answer[0] = 1;
+            answer[1] = 6;
+        }
+        else if(zero == 0 && count == 0){
+            answer[0] = 6;
+            answer[1] = 6;
+        }
+        else{
+            answer[0] = Math.abs(count + zero - 6) + 1;
+            answer[1] = Math.abs(count - 6) + 1;
+        }
+        return answer;
+    }
+}
