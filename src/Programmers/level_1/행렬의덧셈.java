73e425028cd5ed1d54fbc077825d678723b006fb
이준 <ljun1118@ks.ac.kr>
2022-03-16 21:13:16 +0900

    프로그래머스 : 행렬의 덧셈 풀이 완료

--- src/Programmers/level_1/행렬의덧셈.java
+++ src/Programmers/level_1/행렬의덧셈.java
@@ -17,20 +17,15 @@ public class 행렬의덧셈 {
     public static int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = {};
 
-        int loop = arr1.length;
-        answer = new int[loop][loop];
+        int row = arr1.length;
+        int column = arr1[0].length;
 
-        for (int i = 0; i < loop; i++) {
-            for (int j = 0; j < loop; j++) {
-                System.out.println("loop");
-                System.out.println("i = " + i);
-                System.out.println("j = " + j);
-                System.out.println();
-
-                //answer[i][j] = arr1[i][j] + arr2[i][j];
+        answer = new int[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
-
 
         return answer;
     }
