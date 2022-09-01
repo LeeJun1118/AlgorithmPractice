7135da08f93e71f2baad7d87ee7adfaa32d2f38f
ijun <ljun1118@ks.ac.kr>
2022-09-01 22:29:35 +0900

    프로그래머스 : 문자열 내 맘대로 정렬하기 - 좋은 코드

---
+++ src/Programmers/level_1/문자열내맘대로정렬하기.java
@@ -0,0 +1,21 @@
+package Programmers.level_1;
+
+public class 문자열내맘대로정렬하기 {
+    public static void main(String[] args) {
+        solution(new String[]{"a", "df"},2);
+    }
+
+    public static String[] solution(String[] strings, int n) {
+        String[] answer = {};
+
+        for (int i = 0; i < strings.length -1; i++) {
+            for (int j = 1; j < strings.length; j++) {
+
+                String temp = strings[i];
+
+            }
+        }
+
+        return answer;
+    }
+}
